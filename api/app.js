e1834b8029049d05dce08d6d3bad49049bbd80c6
Gabriel L <gabrielleandrojunior@live.com>
2019-12-05 14:23:43 -0300

    cors, imports

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
-import indexRouter from "./routes/index";
+import indexRouter from "./routes/api";
 
 const app = express();
 
@@ -12,10 +12,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use(cors());
+app.use("/api", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
