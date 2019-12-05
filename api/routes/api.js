ea918d358996c7c41f6cb5dd08c984217363b2ee
Gabriel L <gabrielleandrojunior@live.com>
2019-12-05 17:00:08 -0300

    convert api

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,7 +3,7 @@ const router = Router();
 
 router.post("/convert", function(req, res, next) {
   const { code, message } = req.body;
-  res.json({ code, message });
+  res.json({ code: "passou aqui code", message: "passou aqui message" });
 });
 
 export default router;
