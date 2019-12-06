81f90325970665cd9bab0f067d89921fa943b1d9
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 15:32:06 -0300

    add validation

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 const router = Router();
 
-router.post("/convert", function(req, res, next) {
+router.post("/convert", function (req, res, next) {
   const { code, message } = req.body;
-  res.json({ code: "passou aqui code", message: "passou aqui message" });
+
+  if (!code && !message) {
+    res.status(400);
+    res.json({
+      error: "O campo mensagem é obrigatório quando o código não está presente."
+    });
+  } else {
+    res.json({ code: "passou aqui code", message: "passou aqui message" });
+  }
 });
 
 export default router;
