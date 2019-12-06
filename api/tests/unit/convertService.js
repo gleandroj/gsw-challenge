edbfcc3eca155e150c2c897049af8d2bd25d29f1
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 16:27:35 -0300

    folder structure, add convert service test

--- api/tests/unit/convertService.js
+++ api/tests/unit/convertService.js
@@ -5,9 +5,15 @@ chai.should();
 const expect = chai.expect;
 
 describe("ConvertService", () => {
-    describe("POST /convert", () => {
+    describe("messageToCode", () => {
         it("when calls with 'TESTE DE MESA' should returns '833777783303_33063377772'", () => {
             expect(ConvertService.messageToCode('TESTE DE MESA')).to.be.equals('833777783303_33063377772')
+        });
+    });
+
+    describe("codeToMessage", () => {
+        it("when calls with '833777783303_33063377772' should returns 'TESTE DE MESA'", () => {
+            expect(ConvertService.codeToMessage('833777783303_33063377772')).to.be.equals('TESTE DE MESA')
         });
     });
 });
