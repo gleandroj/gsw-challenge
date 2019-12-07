6068a5da2c81fdc481c155c4e7a88de39030361d
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 22:47:10 -0300

    fn

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-router.post("/convert", function (req, res, next) {
+router.post("/convert", (req, res, next) => {
   const { code, message } = req.body;
 
   if (!code && !message) {
