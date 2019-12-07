562051e5cd3038b067217c6e2d4a08803e614d3e
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 22:47:31 -0300

    remove comments

--- api/src/helpers.js
+++ api/src/helpers.js
@@ -5,12 +5,10 @@ export function normalizePort(val) {
     const port = parseInt(val, 10);
 
     if (isNaN(port)) {
-        // named pipe
         return val;
     }
 
     if (port >= 0) {
-        // port number
         return port;
     }
 
