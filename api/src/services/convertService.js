df03ce444e58554f90595fc973279bbb53b4d18f
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 21:35:12 -0300

    remove ctor, ident

--- api/src/services/convertService.js
+++ api/src/services/convertService.js
@@ -1,31 +1,31 @@
 const table = {
-    "A": "2",
-    "B": "22",
-    "C": "222",
-    "D": "3",
-    "E": "33",
-    "F": "333",
-    "G": "4",
-    "H": "44",
-    "I": "444",
-    "J": "5",
-    "K": "55",
-    "L": "555",
-    "M": "6",
-    "N": "66",
-    "O": "666",
-    "P": "7",
-    "Q": "77",
-    "R": "777",
-    "S": "7777",
-    "T": "8",
-    "U": "88",
-    "V": "888",
-    "W": "9",
-    "X": "99",
-    "Y": "999",
-    "Z": "9999",
-    " ": "0"
+  A: "2",
+  B: "22",
+  C: "222",
+  D: "3",
+  E: "33",
+  F: "333",
+  G: "4",
+  H: "44",
+  I: "444",
+  J: "5",
+  K: "55",
+  L: "555",
+  M: "6",
+  N: "66",
+  O: "666",
+  P: "7",
+  Q: "77",
+  R: "777",
+  S: "7777",
+  T: "8",
+  U: "88",
+  V: "888",
+  W: "9",
+  X: "99",
+  Y: "999",
+  Z: "9999",
+  " ": "0"
 };
 
 const tableKeys = Object.keys(table);
@@ -33,49 +33,46 @@ const tableKeys = Object.keys(table);
 const separator = "_";
 
 export default class ConverService {
-    constructor() { }
+  static codeToMessage(code) {
+    const input = code.toLowerCase();
+    let output = "";
+    let charCode = "";
 
-    static codeToMessage(code) {
-        const input = code.toLowerCase();
-        let output = "";
-        let charCode = "";
+    for (let i = 0; i <= input.length; i++) {
+      const nCode = input.charAt(i);
+      const lnCode = charCode.charAt(charCode.length - 1);
 
-        for (let i = 0; i <= input.length; i++) {
-            const nCode = input.charAt(i);
-            const lnCode = charCode.charAt(charCode.length - 1);
+      if (lnCode != "" && (nCode != lnCode || nCode == separator)) {
+        const char = tableKeys.find(key => table[key] === charCode);
+        charCode = "";
+        output = output.concat(char);
 
-            if (lnCode != '' && (nCode != lnCode || nCode == separator)) {
-                const char = tableKeys.find(key => table[key] === charCode);
-                charCode = "";
-                output = output.concat(char);
+        if (nCode === separator) continue;
+      }
 
-                if (nCode === separator) continue;
-            }
-
-            charCode = charCode.concat(nCode);
-        }
-
-        return output;
+      charCode = charCode.concat(nCode);
     }
 
-    static messageToCode(message) {
-        const input = message.toUpperCase();
-        let output = "";
+    return output;
+  }
 
-        for (let i = 0; i < input.length; i++) {
-            const key = input.charAt(i);
-            const code = table[key];
-            const fCode = code.charAt(0);
-            const lCode = output.charAt(output.length - 1);
+  static messageToCode(message) {
+    const input = message.toUpperCase();
+    let output = "";
 
-            if (fCode === lCode) {
-                output = output.concat(separator);
-            }
+    for (let i = 0; i < input.length; i++) {
+      const key = input.charAt(i);
+      const code = table[key];
+      const fCode = code.charAt(0);
+      const lCode = output.charAt(output.length - 1);
 
-            output = output.concat(code);
-        }
+      if (fCode === lCode) {
+        output = output.concat(separator);
+      }
 
-        return output;
+      output = output.concat(code);
     }
 
+    return output;
+  }
 }
