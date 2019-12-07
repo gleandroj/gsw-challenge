e7de03c3b0343919688dbf06c16d37c1253a18ad
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 11:59:02 -0300

    add mask

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -5,6 +5,7 @@ import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 import { Formik } from "formik";
 import * as Yup from "yup"
+import MaskedInput from 'react-text-mask';
 
 const classes = theme => ({
   form: {
@@ -34,6 +35,34 @@ const validationSchema = Yup.object({
   })
 });
 
+const mask = (input) => {
+  const digitReg = /[2-9]|[0]/;
+  const underReg = /\_/;
+
+  return input.split('').map((char, index) => {
+    if (digitReg.test(char)) {
+      return digitReg;
+    } else if (underReg.test(char)) {
+      const lc = input.charAt(index - 1);
+      return underReg.test(lc) ? null : underReg;
+    }
+  });
+};
+
+const TextMaskCustom = ({ inputRef, ...other }) => {
+  return (
+    <MaskedInput
+      {...other}
+      ref={ref => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={mask}
+      placeholderChar={'\u2000'}
+      showMask
+    />
+  );
+}
+
 const MaterialForm = (props) => {
 
   const {
@@ -55,49 +84,70 @@ const MaterialForm = (props) => {
     setFieldTouched(name, true, false);
   };
 
-  return (<form
-    onSubmit={handleSubmit}
-    className={classes.form}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField
-      id="code"
-      label="Código"
-      variant="filled"
-      helperText={touched.code ? errors.code : ""}
-      error={!isValid || !!error}
-      value={code}
-      onChange={change.bind(null, "code")}
-      fullWidth
-      multiline={true}
-      rows={4}
-    />
-    <div className={classes.buttonContainer}>
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        disabled={!isValid}
-        endIcon={<Icon>compare_arrows</Icon>}
-      >
-        Converter e Salvar
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={classes.form}
+      noValidate
+      autoComplete="off"
+    >
+      {/* <TextField
+        id="code"
+        label="Código"
+        variant="filled"
+        inputProps={{ style: { textTransform: 'uppercase' } }}
+        helperText={touched.code ? errors.code : ""}
+        error={!isValid || !!error}
+        value={code}
+        onChange={change.bind(null, "code")}
+        fullWidth
+        multiline={true}
+        rows={4}
+      /> */}
+
+      <TextField
+        id="code"
+        label="Código"
+        variant="filled"
+        inputProps={{ style: { textTransform: 'uppercase' } }}
+        helperText={touched.code ? errors.code : ""}
+        error={!isValid || !!error}
+        value={code}
+        onChange={change.bind(null, "code")}
+        fullWidth
+        multiline={true}
+        rows={4}
+        InputProps={{
+          inputComponent: TextMaskCustom,
+        }}
+      />
+
+      <div className={classes.buttonContainer}>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          disabled={!isValid}
+          endIcon={<Icon>compare_arrows</Icon>}
+        >
+          Converter e Salvar
       </Button>
-    </div>
-    <TextField
-      id="message"
-      label="Mensagem de texto"
-      variant="filled"
-      helperText={touched.message ? errors.message : (error ? error : "")}
-      error={!isValid || !!error}
-      value={message}
-      onChange={change.bind(null, "message")}
-      fullWidth
-      multiline={true}
-      rows={4}
-      inputProps={{ maxLength: 255 }}
-    />
-  </form>);
+      </div>
+      <TextField
+        id="message"
+        label="Mensagem de texto"
+        variant="filled"
+        helperText={touched.message ? errors.message : (error ? error : "")}
+        error={!isValid || !!error}
+        value={message}
+        onChange={change.bind(null, "message")}
+        fullWidth
+        multiline={true}
+        rows={4}
+        inputProps={{ style: { textTransform: 'uppercase' }, ...{ maxLength: 255 } }}
+      />
+    </form>
+  );
 }
 
 class Form extends Component {
