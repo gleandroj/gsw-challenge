66661e62c79261bbb1eca9535dcb097d1e1c2fde
Gabriel L <gabrielleandrojunior@live.com>
2019-12-06 21:32:35 -0300

    Melhorias

--- api/src/app.js
+++ api/src/app.js
@@ -5,31 +5,71 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
 import indexRouter from "./routes/api";
+import { connectDb } from "./models";
+import { normalizePort } from "./helpers";
 
-const app = express();
+export default class Application {
+  constructor() {
+    this.port = normalizePort(process.env.PORT || "8000");
+    this.public_dir = "public";
+    this.app = express();
+    this.isDev = this.app.get("env") === "development";
+  }
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+  async boostrap() {
+    await connectDb();
+    this.app.use(logger("dev"));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(cors());
+    this.app.use(express.static(path.join(__dirname, this.public_dir)));
+    this.apiRoutes();
+    this.app.use(this.catch404.bind(this));
+    this.app.use(this.errorHandler.bind(this));
+    this.app.on("error", this.onError.bind(this));
+  }
 
-app.use("/api", indexRouter);
+  apiRoutes() {
+    this.app.use("/api", indexRouter);
+    this.app.get("/api/ping", (req, res) => res.send("pong"));
+  }
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+  catch404(req, res, next) {
+    next(createError(404));
+  }
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  const error = req.app.get("env") === "development" ? err : {};
+  errorHandler(err, req, res, next) {
+    const error = this.isDev ? err : {};
+    res.status(err.status || 500);
+    res.json(error);
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json(error);
-});
+  onError(error) {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
 
-export default app;
+    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+  listen() {
+    this.app.listen(
+      this.port,
+      console.log.apply(`App listening on port: ${this.port}`)
+    );
+  }
+}
--- api/src/index.js
+++ api/src/index.js
@@ -1,65 +1,18 @@
-import app from "./app";
-import debugCtor from "debug";
-import http from "http";
-import { connectDb } from "./models";
-import { normalizePort } from "./helpers";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import Application from "./app";
 
-const debug = debugCtor("api:server");
+const application = new Application();
 
 /**
- * Get port from environment and store in Express.
+ * Boostrap Application Services
+ * Eg: DB, Etc..
  */
-const port = normalizePort(process.env.PORT || "8000");
-app.set("port", port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Event listener for HTTP server "error" event.
- */
-const onError = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
-}
-
-/**
- * Connect to database
- */
-connectDb().then(() => {
+application.boostrap().then(() => {
   /**
-   * Listen on provided port, on all network interfaces.
+   * Listen app requests
    */
-  server.listen(port);
-  server.on("error", onError);
-  server.on("listening", onListening);
+  application.listen();
 });
 
-export default server;
+export default application.app;
