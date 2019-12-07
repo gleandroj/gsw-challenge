1b6c34099a3a6628599390ec422b373d09e8ffc4
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 16:41:41 -0300

    array-callback-return

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -4,8 +4,8 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 import { Formik } from "formik";
-import * as Yup from "yup"
-import MaskedInput from 'react-text-mask';
+import * as Yup from "yup";
+import MaskedInput from "react-text-mask";
 
 const classes = theme => ({
   form: {
@@ -26,26 +26,28 @@ const useStyle = makeStyles(classes);
 
 const validationSchema = Yup.object({
   code: Yup.string(),
-  message: Yup.string().when('code', {
+  message: Yup.string().when("code", {
     is: code => !code || code.length === 0,
     then: Yup.string()
-      .required('Digite a mensagem ou o código.')
-      .typeError('Digite a mensagem ou o código.'),
+      .required("Digite a mensagem ou o código.")
+      .typeError("Digite a mensagem ou o código."),
     otherwise: Yup.string()
   })
 });
 
-const mask = (input) => {
+const mask = input => {
   const digitReg = /[2-9]|[0]/;
   const underReg = /_/;
 
-  return input.split('').map((char, index) => {
+  return input.split("").map((char, index) => {
     if (digitReg.test(char)) {
       return digitReg;
     } else if (underReg.test(char)) {
       const lc = input.charAt(index - 1);
       return underReg.test(lc) ? null : underReg;
     }
+
+    return null;
   });
 };
 
@@ -57,14 +59,13 @@ const TextMaskCustom = ({ inputRef, ...other }) => {
         inputRef(ref ? ref.inputElement : null);
       }}
       mask={mask}
-      placeholderChar={'\u2000'}
+      placeholderChar={"\u2000"}
       showMask
     />
   );
-}
+};
 
-const MaterialForm = (props) => {
-
+const MaterialForm = props => {
   const {
     values: { code, message },
     errors,
@@ -95,14 +96,14 @@ const MaterialForm = (props) => {
         id="code"
         label="Código"
         variant="filled"
-        inputProps={{ style: { textTransform: 'uppercase' } }}
+        inputProps={{ style: { textTransform: "uppercase" } }}
         helperText={touched.code ? errors.code : ""}
         error={!isValid || !!error}
         value={code}
         onChange={change.bind(null, "code")}
         fullWidth
         InputProps={{
-          inputComponent: TextMaskCustom,
+          inputComponent: TextMaskCustom
         }}
       />
 
@@ -115,24 +116,27 @@ const MaterialForm = (props) => {
           endIcon={<Icon>compare_arrows</Icon>}
         >
           Converter e Salvar
-      </Button>
+        </Button>
       </div>
       <TextField
         id="message"
         label="Mensagem de texto"
         variant="filled"
-        helperText={touched.message ? errors.message : (error ? error : "")}
+        helperText={touched.message ? errors.message : error ? error : ""}
         error={!isValid || !!error}
         value={message}
         onChange={change.bind(null, "message")}
         fullWidth
         multiline={true}
         rows={4}
-        inputProps={{ style: { textTransform: 'uppercase' }, ...{ maxLength: 255 } }}
+        inputProps={{
+          style: { textTransform: "uppercase" },
+          ...{ maxLength: 255 }
+        }}
       />
     </form>
   );
-}
+};
 
 class Form extends Component {
   render() {
