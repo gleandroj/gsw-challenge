45773575c77a7633e8ef6354aad332ef466777a0
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 21:21:33 -0300

    remove copy

--- front/src/components/ConversionTable.test copy.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ConversionTable from "./ConversionTable";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<ConversionTable />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
