6b62b5edfb990412e1462678bb86ef53f23eb012
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 21:23:06 -0300

    export

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Form } from "./Form";
 export { default as TopBar } from "./TopBar";
+export { default as ConversionTable } from "./ConversionTable";
+export { default as TablePaginationActions } from "./TablePaginationActions";
