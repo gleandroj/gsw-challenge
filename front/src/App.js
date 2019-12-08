fc087292da377e5eb21c2b7577d5be848d97ba8c
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 22:40:25 -0300

    refactor

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import { bindActionCreators, compose } from "redux";
-import { addConversion } from "./actions";
-import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Paper from "@material-ui/core/Paper";
-import Table from "./components/ConversionTable";
 import Grid from "@material-ui/core/Grid";
 
-import { Form, TopBar } from "./components";
+import ConversionForm from "./pages/ConversionForm";
+import ConversionsTable from "./pages/ConversionsTable";
 
 const classes = theme => ({
   root: {
@@ -28,14 +23,6 @@ const classes = theme => ({
     flex: 1,
     flexWrap: "wrap"
   },
-  paper: {
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-    position: "relative",
-    flex: 1,
-    height: "100%"
-  },
   grid: {
     [theme.breakpoints.down("md")]: {
       marginBottom: 30
@@ -44,60 +31,22 @@ const classes = theme => ({
       marginLeft: 15,
       marginRight: 15
     }
-  },
-  loadingContainer: {
-    position: "absolute",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 0,
-    zIndex: 10,
-    backgroundColor: "rgba(255, 255, 255, .9)",
-    height: "100%",
-    width: "100%"
   }
 });
 
-const rows = [
-  // {
-  //   code: "2",
-  //   message: "A",
-  //   _id: "id"
-  // }
-];
-
 class App extends Component {
   render() {
-    const { classes, addConversion, pending, error, ...formValue } = this.props;
+    const { classes } = this.props;
     return (
       <div className={classes.root}>
         <CssBaseline />
         <div className={classes.content}>
           <Container maxWidth="lg" className={classes.container}>
             <Grid className={classes.grid} item xs={12} sm={12} md={4}>
-              <Paper className={classes.paper}>
-                <TopBar title="Conversão" />
-                <Form
-                  onSubmit={addConversion}
-                  error={error}
-                  value={formValue}
-                />
-                {pending ? (
-                  <div className={classes.loadingContainer}>
-                    <CircularProgress />
-                    <span>Carregando...</span>
-                  </div>
-                ) : (
-                  []
-                )}
-              </Paper>
+              <ConversionForm />
             </Grid>
             <Grid className={classes.grid} item xs={12} sm={12} md={7}>
-              <Paper className={classes.paper}>
-                <TopBar title="Histórico" />
-                <Table />
-              </Paper>
+              <ConversionsTable />
             </Grid>
           </Container>
         </div>
@@ -106,17 +55,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = store => ({
-  code: store.conversionState.code,
-  message: store.conversionState.message,
-  pending: store.conversionState.pending,
-  error: store.conversionState.error
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ addConversion }, dispatch);
-
-export default compose(
-  withStyles(classes),
-  connect(mapStateToProps, mapDispatchToProps)
-)(App);
+export default withStyles(classes)(App);
