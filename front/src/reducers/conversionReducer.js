e10e35f2fc0138580c43a4c4580a1fe765ad6f18
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 22:40:35 -0300

    add conversions

--- front/src/reducers/conversionReducer.js
+++ front/src/reducers/conversionReducer.js
@@ -1,17 +1,20 @@
 import {
   ADD_CONVERSION_PENDING,
   ADD_CONVERSION_SUCCESS,
-  ADD_CONVERSION_ERROR
+  ADD_CONVERSION_ERROR,
+  FETCH_CONVERSIONS_SUCCESS,
+  FETCH_CONVERSIONS_PENDING,
+  FETCH_CONVERSIONS_ERROR
 } from "../actions/actionTypes";
 
-const initialState = {
+const addInitialState = {
   code: "",
   message: "",
   pending: false,
   error: null
 };
 
-export const conversionReducer = (state = initialState, action) => {
+export const conversionReducer = (state = addInitialState, action) => {
   switch (action.type) {
     case ADD_CONVERSION_ERROR:
       return {
@@ -31,6 +34,42 @@ export const conversionReducer = (state = initialState, action) => {
         ...state,
         code: action.code,
         message: action.message,
+        pending: false,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+const conversionsInitialState = {
+  page: 0,
+  perPage: 5,
+  total: 0,
+  data: [],
+  pending: false,
+  error: null
+};
+
+export const conversionsReducer = (state = conversionsInitialState, action) => {
+  switch (action.type) {
+    case FETCH_CONVERSIONS_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.error
+      };
+    case FETCH_CONVERSIONS_PENDING:
+      return {
+        ...state,
+        page: action.page,
+        perPage: action.perPage,
+        pending: true
+      };
+    case FETCH_CONVERSIONS_SUCCESS:
+      return {
+        ...state,
+        ...action.data,
         pending: false,
         error: null
       };
