3c279c435c70edbbf0853f9e6f86dc25673d60dc
Gabriel L <gabrielleandrojunior@live.com>
2019-12-07 23:24:51 -0300

    add new reducer

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
-import { conversionReducer } from "./conversionReducer";
-import thunk from 'redux-thunk';
+import { conversionReducer, conversionsReducer } from "./conversionReducer";
+import thunk from "redux-thunk";
 
 export const Reducers = combineReducers({
-  conversionState: conversionReducer
+  conversionState: conversionReducer,
+  conversionsState: conversionsReducer
 });
 
-export default createStore(
-  Reducers,
-  applyMiddleware(thunk)
-);
+export default createStore(Reducers, applyMiddleware(thunk));
