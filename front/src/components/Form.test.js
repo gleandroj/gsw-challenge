623cf56aca98587df29161bb8c400b072332fce7
Gabriel L <gabrielleandrojunior@live.com>
2019-12-09 13:04:44 -0300

    tests

--- front/src/components/Form.test.js
+++ front/src/components/Form.test.js
@@ -1,9 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import expect from 'expect';
+import { render, fireEvent } from "@testing-library/react";
 import Form from './Form';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Form />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe("Form", () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("renders without crashing", () => {
+    render(<Form />, container);
+  });
+
+  it("should mask code field correct", () => {
+    const value = {
+      code: "89_9INVALIDO",
+      message: ""
+    };
+
+    act(() => {
+      render(<Form value={value} />, container);
+    });
+
+    const input = document.getElementById('code');
+    expect(input).toBeTruthy();
+    expect(input.value).toEqual("89_9");
+  });
+
+  it("should handle onSubmit when submit form", async () => {
+    const onSubmit = jest.fn();
+    const value = {
+      code: "89",
+      message: ""
+    };
+
+    act(() => {
+      render(<Form value={value} onSubmit={onSubmit} />, container);
+    });
+
+    const button = document.querySelector('button[type="submit"]');
+    expect(button).toBeTruthy();
+    await act(async () => {
+      fireEvent.click(button);
+    });
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit.mock.calls[0][0]).toEqual(value);
+  });
+
 });
