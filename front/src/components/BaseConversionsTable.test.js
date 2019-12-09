30b29270f54b5f33ba120aa1fe3ea1674a2ab195
Gabriel L <gabrielleandrojunior@live.com>
2019-12-09 12:28:16 -0300

    add tests

--- front/src/components/BaseConversionsTable.test.js
+++ front/src/components/BaseConversionsTable.test.js
@@ -1,9 +1,107 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import BaseConversionsTable from "./BaseConversionsTable";
+import expect from 'expect';
+import { render, fireEvent } from "@testing-library/react";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<BaseConversionsTable />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe("BaseConversionsTable", () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(<BaseConversionsTable />, div);
+    unmountComponentAtNode(div);
+  });
+
+  it("handle onchange when click pagination button", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = 5;
+    const total = 10;
+
+    act(() => {
+      render(<BaseConversionsTable page={page} perPage={perPage} total={total} onChange={onChange} />, container);
+    });
+
+    const button = document.querySelector('[aria-label="next page"]');
+    expect(button.children[0].children[0].innerHTML).toBe("keyboard_arrow_right");
+
+    fireEvent.click(button);
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toBeCalledWith({ page: 1, perPage });
+  });
+
+  it("should show total when selected per page is -1", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = -1;
+    const total = 10;
+
+    act(() => {
+      render(<BaseConversionsTable page={page} perPage={perPage} total={total} onChange={onChange} />, container);
+    });
+
+    const elements = document.getElementsByClassName("MuiTypography-root MuiTablePagination-caption MuiTypography-body2 MuiTypography-colorInherit");
+    expect(elements).toBeTruthy();
+    const p = elements[1];
+    expect(p).toBeTruthy();
+    expect(p.innerHTML).toEqual(`1-${total} de ${total}`);
+  });
+
+  it("handle onchange when change itens per page", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = 5;
+    const total = 10;
+
+    act(() => {
+      render(<BaseConversionsTable page={page} perPage={perPage} total={total} onChange={onChange} />, container);
+    });
+
+    const select = document.querySelector('[aria-label="Qtd. Itens"]');
+    fireEvent.change(select, { target: { value: 10 } });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toBeCalledWith({ page: 0, perPage: 10 });
+  });
+
+  it("should render rows", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = 5;
+    const total = 10;
+    const rows = [
+      {
+        _id: "1",
+        code: "8",
+        message: "G"
+      }
+    ];
+
+    act(() => {
+      render(<BaseConversionsTable rows={rows} page={page} perPage={perPage} total={total} onChange={onChange} />, container);
+    });
+
+    const body = document.getElementsByClassName("MuiTableBody-root")[0];
+    expect(body).toBeTruthy();
+    const tr = body.children[0];
+    expect(tr).toBeTruthy();
+    expect(tr.children[0]).toBeTruthy();
+    expect(tr.children[0].innerHTML).toEqual(rows[0].code);
+    expect(tr.children[1].innerHTML).toEqual(rows[0].message);
+  });
 });
--- front/src/components/TablePaginationActions.test.js
+++ front/src/components/TablePaginationActions.test.js
@@ -1,9 +1,95 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import expect from 'expect';
+import { render, fireEvent } from "@testing-library/react";
 import TablePaginationActions from "./TablePaginationActions";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<TablePaginationActions />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe("TablePaginationActions", () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("renders without crashing", () => {
+    render(<TablePaginationActions />, container);
+  });
+
+  it("handle onchange when click next page button", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = 5;
+    const total = 10;
+
+    act(() => {
+      render(<TablePaginationActions page={page} count={total} rowsPerPage={perPage} onChangePage={onChange} />, container);
+    });
+
+    const button = document.querySelector('[aria-label="next page"]');
+    expect(button.children[0].children[0].innerHTML).toBe("keyboard_arrow_right");
+    fireEvent.click(button);
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][1]).toBe(1);
+  });
+
+  it("handle onchange when click prev page button", () => {
+    const onChange = jest.fn();
+    const page = 1;
+    const perPage = 5;
+    const total = 10;
+
+    act(() => {
+      render(<TablePaginationActions page={page} count={total} rowsPerPage={perPage} onChangePage={onChange} />, container);
+    });
+
+    const button = document.querySelector('[aria-label="previous page"]');
+    expect(button.children[0].children[0].innerHTML).toBe("keyboard_arrow_left");
+    fireEvent.click(button);
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][1]).toBe(0);
+  });
+
+  it("handle onchange when click last page button", () => {
+    const onChange = jest.fn();
+    const page = 0;
+    const perPage = 5;
+    const total = 15;
+
+    act(() => {
+      render(<TablePaginationActions page={page} count={total} rowsPerPage={perPage} onChangePage={onChange} />, container);
+    });
+
+    const button = document.querySelector('[aria-label="last page"]');
+    expect(button.children[0].children[0].innerHTML).toBe("last_page");
+    fireEvent.click(button);
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][1]).toBe(2);
+  });
+
+
+  it("handle onchange when click first page button", () => {
+    const onChange = jest.fn();
+    const page = 2;
+    const perPage = 5;
+    const total = 15;
+
+    act(() => {
+      render(<TablePaginationActions page={page} count={total} rowsPerPage={perPage} onChangePage={onChange} />, container);
+    });
+
+    const button = document.querySelector('[aria-label="first page"]');
+    expect(button.children[0].children[0].innerHTML).toBe("first_page");
+    fireEvent.click(button);
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][1]).toBe(0);
+  });
 });
